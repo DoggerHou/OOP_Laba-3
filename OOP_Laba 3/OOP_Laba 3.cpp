36cf79c520abb1da973b2268aab3cd37d72341c3
Artem <Artem@DESKTOP-5RBGIAD>
2022-11-06 22:50:50 +0500

    Добавьте файлы проекта.

---
+++ OOP_Laba 3/OOP_Laba 3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+
+class Animal 
+{
+private:
+    string name;
+public:
+    virtual void sound() = 0;
+};
+
+class Dog: public Animal 
+{
+public:
+    //Dog() { cout << "Dog()"; }
+    void sound() override 
+    {
+        cout<<"bark-bark\n";
+    }  
+    void chaseCat() {}
+};
+
+class Cat : public Animal 
+{
+public:
+    void sound() override 
+    {
+        cout << "mew-mew\n";
+    }
+    void catchMouse() {}
+};
+
+int main()
+{
+    unsigned int Start_Time = clock();
+    setlocale(LC_ALL,"rus");
+    Animal* storage1[100];
+    unsigned int End_Time = clock();
+    storage1[0] = new Dog();
+    cout <<"\nВремя выполнения программы:"<< End_Time - Start_Time;
+}
