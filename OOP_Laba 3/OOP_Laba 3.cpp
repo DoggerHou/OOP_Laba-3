e0709323d79d86d5dc22f40f236ee2ac306d0ae8
DoggerHou <40437811+DoggerHou@users.noreply.github.com>
2022-11-07 11:22:58 +0500

    Update OOP_Laba 3.cpp

--- OOP_Laba 3/OOP_Laba 3.cpp
+++ OOP_Laba 3/OOP_Laba 3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <time.h>
 #include <Windows.h>
 using namespace std;
 
@@ -12,14 +13,13 @@ public:
         cout << "~BaseClass()\n";
     }
     virtual void ShowName() = 0;
-    virtual bool isA(const string& who) {
-        return (who == "BaseClass");
-    }
 };
 
 class People : public BaseClass {
 private:
-    string name = "Chelovek";
+    string name = "Human";
+    int weight;
+    int length;
 public:
     People() {
         cout << "People()\n";
@@ -30,12 +30,13 @@ public:
     virtual void ShowName() override {
         cout << name;
     }
-    virtual bool isA(const string& who) override {
-        return (who == "People") || (who == "BaseClass");
+    People(int weigth, int heigth) {
+        this->weight = weigth;
+        this->length = length;
     }
 };
 
-class Metal:public BaseClass {
+class Metal : public BaseClass {
 private:
     string name = "Iron";
 public:
@@ -48,8 +49,8 @@ public:
     virtual void ShowName() override {
         cout << name;
     }
-    virtual bool isA(const string& who) override {
-        return (who == "Metal") || (who == "BaseClass");
+    void MetalNazi() {
+        cout << "Nazi\n";
     }
 };
 
@@ -58,18 +59,35 @@ private:
     BaseClass** objects;        //массив указателей на указатели типа BaseClass
     int size;                   //размер массива
 public:
-    Storage(int size) {         //Конструктор(задаем размер при создании
+    Storage(int size) {         //Конструктор(задаем размер при создании)
         this->size = size;
         objects = new BaseClass * [size];
     }
     void SetObject(int index, BaseClass* object) {//помещаем объект
+        if (index + 1 > size)
+            Resize();
         objects[index] = object;
     }
-    BaseClass &GetObjectClass(int index) {      //возвращаем объект дочернего класса
-        return *objects[index];
+    BaseClass *GetObjectClass(int index) {      //возвращаем указатель на объект дочернего класса
+        return objects[index];
     }
     void DeleteObject(int index) {              //удаляем объект
         delete objects[index];
+        for (int i = index + 1; i < size - 1; i++) {//убираем дырки после удаления
+            objects[i] = objects[i + 1];
+        }
+    }
+
+    void Resize() {
+        BaseClass** objects2 = new BaseClass * [size * 2];
+        for (int i = 0; i < size; ++i)
+            objects2[i] = objects[i];
+        for (int i = size; i < size * 2; ++i)
+            objects2[i] = nullptr;
+
+        delete[] objects;
+        objects = objects2;
+        size = size * 2;
     }
 };
 
@@ -78,10 +96,17 @@ int main()
     unsigned int Start_Time = clock();
     setlocale(LC_ALL,"rus");
     unsigned int End_Time = clock();
-    Storage MyStorage(228);
-    for (int i = 0; i < 100; i++) {
-        MyStorage.SetObject(i, new People);
-        MyStorage.DeleteObject(i);
+    srand(time(NULL));
+    Storage MyStorage(200);
+    //MyStorage.SetObject(0, new Metal);
+    //if (dynamic_cast<Metal*>(MyStorage.GetObjectClass(0)))
+    //    dynamic_cast<Metal*>(MyStorage.GetObjectClass(0))->MetalNazi();
+
+    for (int i = 0; i < 1000; i++) {
+        cout << i;
+        MyStorage.SetObject(i, new Metal);
+        dynamic_cast<Metal*>(MyStorage.GetObjectClass(i))->MetalNazi();
+        
     }
     cout <<"\nВремя выполнения программы:"<< End_Time - Start_Time;
 }
