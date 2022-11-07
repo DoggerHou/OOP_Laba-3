a25780f573d4df2ae45f6cb4b79428e82cb9e378
DoggerHou <40437811+DoggerHou@users.noreply.github.com>
2022-11-07 09:28:12 +0500

    Update OOP_Laba 3.cpp

--- OOP_Laba 3/OOP_Laba 3.cpp
+++ OOP_Laba 3/OOP_Laba 3.cpp
@@ -1,43 +1,87 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
 #include <Windows.h>
 using namespace std;
 
-class Animal 
-{
-private:
-    string name;
+class BaseClass {        //базовый абстрактный класс Материалы
 public:
-    virtual void sound() = 0;
-};
-
-class Dog: public Animal 
-{
-public:
-    //Dog() { cout << "Dog()"; }
-    void sound() override 
-    {
-        cout<<"bark-bark\n";
-    }  
-    void chaseCat() {}
-};
-
-class Cat : public Animal 
-{
-public:
-    void sound() override 
-    {
-        cout << "mew-mew\n";
+    BaseClass() {
+        cout << "BaseClass()\n";
     }
-    void catchMouse() {}
+    virtual ~BaseClass() {
+        cout << "~BaseClass()\n";
+    }
+    virtual void ShowName() = 0;
+    virtual bool isA(const string& who) {
+        return (who == "BaseClass");
+    }
+};
+
+class People : public BaseClass {
+private:
+    string name = "Chelovek";
+public:
+    People() {
+        cout << "People()\n";
+    }
+    virtual ~People() {
+        cout << "~People()\n";
+    }
+    virtual void ShowName() override {
+        cout << name;
+    }
+    virtual bool isA(const string& who) override {
+        return (who == "People") || (who == "BaseClass");
+    }
+};
+
+class Metal:public BaseClass {
+private:
+    string name = "Iron";
+public:
+    Metal() {
+        cout << "Metal()\n";
+    }
+    virtual ~Metal() {
+        cout << "~Metal()\n";
+    }
+    virtual void ShowName() override {
+        cout << name;
+    }
+    virtual bool isA(const string& who) override {
+        return (who == "Metal") || (who == "BaseClass");
+    }
+};
+
+class Storage {         //Класс хранилище
+private:
+    BaseClass** objects;        //массив указателей на указатели типа BaseClass
+    int size;                   //размер массива
+public:
+    Storage(int size) {         //Конструктор(задаем размер при создании
+        this->size = size;
+        objects = new BaseClass * [size];
+    }
+    void SetObject(int index, BaseClass* object) {//помещаем объект
+        objects[index] = object;
+    }
+    BaseClass &GetObjectClass(int index) {      //возвращаем объект дочернего класса
+        return *objects[index];
+    }
+    void DeleteObject(int index) {              //удаляем объект
+        delete objects[index];
+    }
 };
 
 int main()
 {
     unsigned int Start_Time = clock();
     setlocale(LC_ALL,"rus");
-    Animal* storage1[100];
     unsigned int End_Time = clock();
-    storage1[0] = new Dog();
+    Storage MyStorage(228);
+    for (int i = 0; i < 100; i++) {
+        MyStorage.SetObject(i, new People);
+        MyStorage.DeleteObject(i);
+    }
     cout <<"\nВремя выполнения программы:"<< End_Time - Start_Time;
 }
